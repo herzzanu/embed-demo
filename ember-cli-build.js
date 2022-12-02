192a3a192715a620bfe7d2c059059e7b705c4e6e
Sabin Hertanu <sabin@apperia.co>
2022-12-02 01:00:00 +0100

    Disable embed

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,5 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return require('ember-embedded-snippet').process(app, app.toTree());
+  return app.toTree();
 };
