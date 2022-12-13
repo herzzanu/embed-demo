06647f9766227fc1ea169903edbc50218d571dd7
Sabin Hertanu <sabin@apperia.co>
2022-12-13 22:56:14 +0100

    Add destination host

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,23 +1,10 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import ENV from 'embed-demo/config/environment';
 
 export default class ApplicationController extends Controller {
   @service embedded;
 
   get isEmbedded() {
-    console.log('App host: ', ENV.APP.appHost);
-    console.log('Origin host: ', this.embedded.originHost);
-
-    return (
-      this.embedded.originHost !== ENV.APP.appHost &&
-      ENV.environment === 'production'
-    );
-  }
-
-  get foo() {
-    return this.embedded.args.hasOwnProperty('enable-foo')
-      ? this.embedded.args.foo
-      : null;
+    return this.embedded.originHost !== window.location.origin;
   }
 }
